8c8ff9908b266b63e6b15b35d4baf508bbe2a367
dosheena93 <dosheena93@gmail.com>
2024-01-21 23:50:56 -0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+// Import only what is needed
+import React from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div>
+      
+      </div>
+      <h1>hi</h1>
+      <p className="read-the-docs">
+       </p>
+    </>
+  );
+}
+
+export default App;
